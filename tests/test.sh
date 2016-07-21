b0cb873cd337e7772a657eb03a69660738ffe133
Greg Lindahl <lindahl@pbm.com>
2016-07-21 16:34:36 -0700

    turn on test coverage plus coveralls

--- tests/test.sh
+++ tests/test.sh
@@ -6,9 +6,12 @@ set -e
 # start a webserver
 python ./mock-webserver.py > /dev/null 2>&1 &
 
+# if COVERAGE is set, use it, else python
+if [ -z "$COVERAGE"]; then COVERAGE=python; fi
+
 echo test-deep
 echo
-python ../cocrawler/crawl.py --configfile test-deep.yml
+$COVERAGE ../cocrawler/crawl.py --configfile test-deep.yml
 # tests against the logfiles
 grep -q "/denied/" robotslog.jsonl || (echo "FAIL: nothing about /denied/ in robotslog"; exit 1)
 (grep "/denied/" crawllog.jsonl | grep -q -v '"robots"' ) && (echo "FAIL: should not have seen /denied/ in crawllog.jsonl"; exit 1)
@@ -16,7 +19,7 @@ grep -q "/denied/" robotslog.jsonl || (echo "FAIL: nothing about /denied/ in rob
 echo
 echo test-wide
 echo
-python ../cocrawler/crawl.py --configfile test-wide.yml
+$COVERAGE ../cocrawler/crawl.py --configfile test-wide.yml
 
 # remove logfiles
 rm -f robotslog.jsonl crawllog.jsonl
