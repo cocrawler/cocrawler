e13fed0ac7e4efea8a965b2f6df8645bd661dea9
Greg Lindahl <lindahl@pbm.com>
2016-07-26 16:50:42 -0700

    improve coverage

--- cocrawler/test_parse.py
+++ cocrawler/test_parse.py
@@ -4,7 +4,7 @@ import parse
 
 test_html = '''
 <html>
-<header><title>Foo</title><link href='link.html'></link></header>
+<head><title>Foo</title><link href='link.html'></link></head>
 <body>
 <a href="foo1.html"></a>
 <a
@@ -12,8 +12,39 @@ href=foo2.htm></a>
 <a
  href="foo3.html "></a>
 <img src=foo.gif />
-<body>
 </body>
+'''
+
+test_html_no_body = '''
+<html>
+<head><title>Foo</title><link href='link.html'></link></head>
+<a href="foo1.html"></a>
+<a
+href=foo2.htm></a>
+<a
+ href="foo3.html "></a>
+<img src=foo.gif />
+'''
+
+test_html_no_head = '''
+<html>
+<body>
+<a href="foo1.html"></a>
+<a
+href=foo2.htm></a>
+<a
+ href="foo3.html "></a>
+<img src=foo.gif />
+</body>
+'''
+
+test_html_no_nothing = '''
+<a href="foo1.html"></a>
+<a
+href=foo2.htm></a>
+<a
+ href="foo3.html "></a>
+<img src=foo.gif />
 '''
 
 def test_html_parse():
@@ -25,6 +56,18 @@ def test_html_parse():
     links, embeds = parse.find_html_links_and_embeds(test_html)
     assert len(links) == 3
     assert len(embeds) == 2
+
+    links, embeds = parse.find_html_links_and_embeds(test_html_no_body)
+    assert len(links) == 3
+    assert len(embeds) == 2
+
+    links, embeds = parse.find_html_links_and_embeds(test_html_no_head)
+    assert len(links) == 3
+    assert len(embeds) == 1
+
+    links, embeds = parse.find_html_links_and_embeds(test_html_no_nothing)
+    assert len(links) == 3
+    assert len(embeds) == 1
 
 test_css = '''
 @import url('foo1.css')
