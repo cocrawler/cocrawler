1ab8490e28342176ef012fcb0dbcf4b228597df2
Greg Lindahl <lindahl@pbm.com>
2016-09-10 15:41:43 -0700

    run parsers on a tree of html

---
+++ cocrawler/run_parsers.py
@@ -0,0 +1,40 @@
+'''
+Runs all of the available parsers over a tree of html
+
+Accumulate cpu time
+Compare counts of urls and embeds
+'''
+
+import sys
+import os
+import logging
+
+import stats
+import parse
+
+def parse_all(name, string):
+    links1 = parse.find_html_links(string)
+    links2, embeds2 = parse.find_html_links_and_embeds(string)
+
+    all2 = links2.union(embeds2)
+
+    if len(links1) != len(all2):
+        print('{} had different link counts of {} and {}'.format(name, len(links1), len(all2)))
+        extra1 = links1.difference(all2)
+        extra2 = all2.difference(links1)
+        print('  extra in links:            {!r}'.format(extra1))
+        print('  extra in links and embeds: {!r}'.format(extra2))
+    return
+
+for d in sys.argv[1:]:
+    for root, _, files in os.walk(d):
+        for name in files:
+            if name.endswith('.html') or name.endswith('.htm'):
+                expanded = os.path.join(root, name)
+                #print('{}:'.format(expanded))
+                with open(expanded, 'r', errors='ignore') as f:
+                    parse_all(name, f.read())
+
+levels = [logging.ERROR, logging.WARN, logging.INFO, logging.DEBUG]
+logging.basicConfig(level=levels[3])
+stats.report()
