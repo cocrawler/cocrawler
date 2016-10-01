241dc5a48bd89d0736294d4ca83f9ac8cae3b77d
Greg Lindahl <lindahl@pbm.com>
2016-10-01 16:46:51 -0700

    better coverage

---
+++ cocrawler/test_burner.py
@@ -0,0 +1,15 @@
+import functools
+import burner
+
+def trivial():
+    return 42,
+
+def test_stats_wrap():
+    '''
+    This code only runs in a separate thread, so pytest doesn't do coverage
+    for it, even though my test suite does test it. Do a trivial test here.
+    '''
+    partial = functools.partial(trivial)
+
+    burner.stats_wrap(trivial, 'trivial')
+
