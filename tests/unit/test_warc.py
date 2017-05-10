9abafa23e49281be0ddaa818bb4bd15af32cc41c
Greg Lindahl <lindahl@pbm.com>
2017-05-09 19:49:32 -0700

    make safe for non-ordered dicts in python 3.5

--- tests/unit/test_warc.py
+++ tests/unit/test_warc.py
@@ -5,18 +5,23 @@ from multidict import CIMultiDict, MultiDict
 
 def test_headers_to_str_headers():
     result = [('foo', 'bar'), ('baz', 'barf')]
+    result2 = [('baz', 'barf'), ('foo', 'bar')]  # because dict order is not in python 3.5
     assert warc.headers_to_str_headers(result) == result
 
     header_dict = {'foo': b'bar', b'baz': 'barf'}
-    assert warc.headers_to_str_headers(header_dict) == result
+    ret = warc.headers_to_str_headers(header_dict)
+    assert ret == result or ret == result2
 
     aiohttp_raw_headers = ((b'foo', b'bar'), (b'baz', b'barf'))
     assert warc.headers_to_str_headers(aiohttp_raw_headers) == result
 
     aiohttp_headers = MultiDict(foo='bar', baz=b'barf')
-    assert warc.headers_to_str_headers(aiohttp_headers) == result
+    ret = warc.headers_to_str_headers(aiohttp_headers)
+    assert ret == result or ret == result2
 
     # This case-insensitive thingie titlecases the key
     aiohttp_headers = CIMultiDict(foo='bar', baz=b'barf')
     titlecase_result = [('Foo', 'bar'), ('Baz', 'barf')]
-    assert warc.headers_to_str_headers(aiohttp_headers) == titlecase_result
+    titlecase_result2 = [('Baz', 'barf'), ('Foo', 'bar')]
+    ret = warc.headers_to_str_headers(aiohttp_headers)
+    assert ret == titlecase_result or ret == titlecase_result2
