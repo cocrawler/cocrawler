1f9e8dbccd7393494782c1c83dd2c915cbab4558
Greg Lindahl <lindahl@pbm.com>
2017-10-19 13:37:40 -0700

    bump; remove deprecated call

--- cocrawler/webserver.py
+++ cocrawler/webserver.py
@@ -34,7 +34,6 @@ def close(app):
     srv.close()
     loop.run_until_complete(srv.wait_closed())
     loop.run_until_complete(app.shutdown())
-    loop.run_until_complete(handler.finish_connections(60.0))
     loop.run_until_complete(app.cleanup())
 
 
