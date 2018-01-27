48a95a315c3f59db0c6589645bbaae4fa76e06d4
Greg Lindahl <lindahl@pbm.com>
2018-01-26 21:32:36 -0800

    seed handling

--- cocrawler/seeds.py
+++ cocrawler/seeds.py
@@ -29,7 +29,7 @@ def expand_seeds_config(crawler):
     if seeds.get('Hosts', []):
         for h in seeds['Hosts']:
             u = special_seed_handling(h)
-            urls.append(u)
+            urls.append((h, u))
 
     seed_files = seeds.get('Files', [])
     if seed_files:
@@ -46,25 +46,41 @@ def expand_seeds_config(crawler):
                     if line == '':
                         continue
                     u = special_seed_handling(line)
-                    urls.append(u)
+                    urls.append((line, u))
 
-    # sitemaps are a little tedious, so I'll implement later.
-    # needs to be fetched and then xml parsed and then <urlset ><url><loc></loc> elements extracted
+    final_urls = []
+    for seed_host, u in urls:
+        url = URL(u)
+        if POLICY == 'www-then-non-www':
+            # url already has a scheme, may or may not have www
+            if url.hostname == url.hostname_without_www:
+                if u.startswith('http://'):
+                    second = u.replace('http://', 'http://www.', 1)
+                elif u.startswith('https://'):
+                    second = u.replace('https://', 'http://www.', 1)  # second chance is always http
+                else:
+                    raise ValueError('invalid seed: '+seed_host+' '+u)
+            else:
+                second = u.replace('://www.', '://', 1)
+                if second == u:
+                    raise ValueError('invalid seed 2: '+seed_host+' '+u)
+        else:
+            second = ''
+        final_urls.append((seed_host, url, second))
+    return seed_some_urls(final_urls, crawler)
 
-    return seed_some_urls(urls, crawler)
 
-
-def seed_some_urls(urls, crawler, second_chance=True):
+def seed_some_urls(urls, crawler):
     freeseedredirs = config.read('Seeds', 'FreeSeedRedirs')
     retries_left = config.read('Seeds', 'SeedRetries') or config.read('Crawl', 'MaxTries')
     priority = 1
 
-    # url_allowed.setup_seeds(urls)  # add_url now does this
-
-    for u in urls:
-        ridealong = {'url': u, 'priority': priority, 'seed': True,
+    for seed_host, url, second_chance_url in urls:
+        ridealong = {'url': url, 'priority': priority, 'seed': True,
                      'skip_seen_url': True, 'retries_left': retries_left,
-                     'original_url': u.url, 'second_chance': second_chance}
+                     'seed_host': seed_host}
+        if second_chance_url:
+            ridealong['second_chance_url'] = second_chance_url
         if freeseedredirs:
             ridealong['free_redirs'] = freeseedredirs
         crawler.add_url(priority, ridealong)
@@ -100,8 +116,6 @@ def special_seed_handling(url):
                 url = url.replace('http://', 'http://www.', 1)
             else:
                 url = url.replace('https://', 'https://www.', 1)
-
-    url = URL(url)
     return url
 
 
@@ -113,30 +127,13 @@ def fail(ridealong, crawler):
         return
 
     url = ridealong['url']
-
-    if not ridealong.get('second_chance', False):
-        return
-    del ridealong['second_chance']
-
     LOGGER.info('Received a final failure for seed url %s', url.url)
     stats.stats_sum('seeds failed', 1)
 
-    if POLICY == 'www-then-non-www':
-        # url could have changed because of a redirect, that's why we saved it
-        if 'original_url' not in ridealong:
-            LOGGER.info('should have seen original_url in this seed, but did not')
-            return
+    if 'second_chance_url' not in ridealong:
+        return
 
-        original_url = ridealong['original_url']
-        if 'www.' not in original_url:
-            LOGGER.info('original url did not contain www, adding it')
-            if original_url.startswith('https://'):
-                url = URL(original_url.replace('https://', 'https://www.', 1))
-            if original_url.startswith('http://'):
-                url = URL(original_url.replace('http://', 'http://www.', 1))
-        else:
-            url = URL(original_url.replace('www.', '', 1))
+    two = ridealong['second_chance_url']
+    seed_host = ridealong.get('seed_host', None)
 
-        LOGGER.info('seed url second chance: %s to %s', original_url, url.url)
-        stats.stats_sum('seeds second chances', 1)
-        seed_some_urls((url,), crawler, second_chance=False)
+    seed_some_urls((seed_host, two, None), crawler)
--- tests/unit/test_seeds.py
+++ tests/unit/test_seeds.py
@@ -3,7 +3,7 @@ import cocrawler.seeds as seeds
 
 def test_special_seed_handling():
     specialsh = seeds.special_seed_handling
-    assert specialsh('foo').url == 'http://foo/'
-    assert specialsh('//foo/').url == 'http://foo/'
-    assert specialsh('https://foo').url == 'https://foo/'
-    #assert specialsh('mailto:foo').url == 'mailto:foo'
+    assert specialsh('foo') == 'http://foo'
+    assert specialsh('//foo/') == 'http://foo/'
+    assert specialsh('https://foo') == 'https://foo'
+    #assert specialsh('mailto:foo') == 'mailto:foo'
