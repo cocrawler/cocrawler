7798bb1a76a77db948cf042d55cb508b06021678
Greg Lindahl <lindahl@pbm.com>
2018-08-26 14:35:40 -0700

    collect rel=shortlink

--- cocrawler/facet.py
+++ cocrawler/facet.py
@@ -41,7 +41,7 @@ meta_property_prefix = (('al:', 'applinks'),
                         ('op:', 'fb instant'),
                         ('bt:', 'boomtrain'),)
 
-link_rel = set(('canonical', 'alternate', 'amphtml', 'opengraph', 'origin', 'next', 'prev', 'previous', 'me'))
+link_rel = set(('canonical', 'alternate', 'amphtml', 'opengraph', 'origin', 'next', 'prev', 'previous', 'me', 'shortlink'))
 
 save_response_headers = ('refresh', 'server', 'set-cookie', 'strict-transport-security', 'tk')
 
